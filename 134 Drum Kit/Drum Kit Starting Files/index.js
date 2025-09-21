4d9519d7352b38e954259767ca9497b210055203
pongpak-TEPHEE <pongpak.te@gmail.com>
2025-09-22 00:41:29 +0700

    adding event listeners

---
+++ 134 Drum Kit/Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+
+for(var i=0; i<document.querySelectorAll("button").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function () { 
+        alert("I got clicked!"); 
+    });
+}
+
+
