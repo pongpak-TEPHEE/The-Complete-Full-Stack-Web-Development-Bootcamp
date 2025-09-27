9b75b33a9863c2747e586f53323fc116e0d40dab
pongpak-TEPHEE <pongpak.te@gmail.com>
2025-09-28 01:01:16 +0700

    Map sound with button

--- 134 Drum Kit/Drum Kit Starting Files/index.js
+++ 134 Drum Kit/Drum Kit Starting Files/index.js
@@ -1,8 +1,25 @@
-
 for(var i=0; i<document.querySelectorAll("button").length; i++){
+    
     document.querySelectorAll("button")[i].addEventListener("click", function () { 
-        alert("I got clicked!"); 
-    });
-}
+        console.log(this.innerHTML);
 
+        const soundMap = {
+            "w": "tom-1",
+            "a": "tom-2",
+            "s": "tom-3",
+            "d": "tom-4",
+            "j": "snare",
+            "k": "crash",
+            "l": "kick-bass"
+        }
+
+        if (soundMap[this.innerHTML]) {
+            var sound = new Audio("sounds/" + soundMap[this.innerHTML] + ".mp3");
+            sound.play();
+        } else {
+            console.log("No sound mapped for this button: " + this.innerHTML);
+        }
+    });
+    
+}
 
